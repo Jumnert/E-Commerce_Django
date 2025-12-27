a33fa3303e71eca5dc1da3825e4477f49ac98bd6
jumnert <chountheachumnith@gmail.com>
2025-12-27 20:01:22 +0700

    Fix: Duplicated Slug

--- store/models.py
+++ store/models.py
@@ -32,24 +32,21 @@ class Category(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=150, verbose_name="Product Title")
-    slug = models.SlugField(max_length=160, verbose_name="Product Slug")
-    sku = models.CharField(max_length=255, unique=True, verbose_name="Unique Product ID (SKU)")
-    short_description = models.TextField(verbose_name="Short Description")
-    detail_description = models.TextField(blank=True, null=True, verbose_name="Detail Description")
-    product_image = models.ImageField(upload_to='product', blank=True, null=True, verbose_name="Product Image")
+    slug = models.SlugField(
+        max_length=160,
+        unique=True,   # ✅ FIX
+        verbose_name="Product Slug"
+    )
+    sku = models.CharField(max_length=255, unique=True)
+    short_description = models.TextField()
+    detail_description = models.TextField(blank=True, null=True)
+    product_image = models.ImageField(upload_to='product', blank=True, null=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
-    category = models.ForeignKey(Category, verbose_name="Product Categoy", on_delete=models.CASCADE)
-    is_active = models.BooleanField(verbose_name="Is Active?")
-    is_featured = models.BooleanField(verbose_name="Is Featured?")
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created Date")
-    updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated Date")
-
-    class Meta:
-        verbose_name_plural = 'Products'
-        ordering = ('-created_at', )
-
-    def __str__(self):
-        return self.title
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    is_active = models.BooleanField()
+    is_featured = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 class Cart(models.Model):
